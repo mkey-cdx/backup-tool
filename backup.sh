22e4b6a52c18d6e3a5f30e8f64ad0f73d5b51b2a
Mickaël Coiraton <mkey@localhost.localdomain>
2016-12-18 10:40:38 +0100

    New file: backup.sh

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ==============================================================================
+# Rsync backup tool
+#
+# Maintainer: Mickaël Coiraton
+# Version 0.0.1
+# Updated: 18 December 2016
+#
+# This is a simple backup tool based on rsync.
+# It creates a full backup and a differential backup for each day of a week.
+#
+# ==============================================================================
