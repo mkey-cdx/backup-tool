bcfbcc33f42405d38386391742559bd4417e18f0
Mickaël Coiraton <mkey@localhost.localdomain>
2016-12-20 11:56:14 +0100

    modified:   backup.sh

--- backup.sh
+++ backup.sh
@@ -7,45 +7,45 @@
 # Version 0.0.1
 # Updated: 18 December 2016
 #
-# This is a simple backup tool based on rsync and SSH.
+# This is a simple rotating backups tool based on rsync and SSH.
 # It backups selected directories from a remote machine to the local machine 
 # which runs the script.
 #
-# IP or FQDN of remote machine
-r_host=remote_host
-# Remote username
-r_user=remote_user
-# Location of passphraseless ssh keyfile
-ssh_key=/path/to/local/ssh_keyfile
-# Directories to backup. Separate with a space. Exclude trailing slash
-sources="/path/to/remote/directory"
-# Directory to backup to on the local machine. Exclude trailing slash.
-target="/path/to/local/backup"
-# Comment out the following line to enable verbose output.
-#verbose="-v"
 # ==============================================================================
 
+# Get backup config.
+source backup.cfg
+
 # SSH keyfile checking.
-if [ ! -f $key ]; then
+if [ ! -f $ssh_key ]; then
   echo "Couldn't find ssh keyfile!"
   echo "Exiting..."
   exit 2
 fi
 
-# Remote permissions checking.
-if ! ssh -i $key $r_user@$r_host "test -x $sources"; then
-  echo "Target directory on remote machine doesn't exist or bad permissions."
-  echo "Exiting..."
-  exit 2
-fi
+# Rsync exclude parameter.
+for dir in $exclude_dirs; do
+    exclude="$exclude --exclude=$dir"
+done
 
-# Set name (date) of backup.
-backup_date="`date +%F_%H-%M`"
+# Running verification and rsync for each source.
+for source in $sources; do
 
-#DAY=$(date +%A)
-#
-#if [ -e /location/to/backup/incr/$DAY ] ; then
-#  rm -fr /location/to/backup/incr/$DAY
-#fi
-#
-#rsync -a --delete --inplace --backup --backup-dir=/location/to/backup/incr/$DAY /folder/to/backup/ /location/to/backup/full/ &> /dev/null
+    # Check remote permissions.
+    if ! ssh -i $ssh_key $r_user@$r_host "test -x $source"; then
+        echo "Error with $source!"
+        echo "Target directory on remote machine doesn't exist or bad permissions."
+        echo "Exiting..."
+        exit 2
+    fi
+
+    # Create local backup directory.
+    if [ -e $backup_dir/$source ]; then
+        mkdir -p $backup_dir/$source
+    fi
+
+    # Run rsync.
+    rsync $exclude -a --delete -e "ssh -i $ssh_key" $r_user@$r_host:$source/ $backup_dir/$source/
+
+done
+
