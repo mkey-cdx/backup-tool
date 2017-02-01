1e0e62ed0d99291615144f1481a215a09757efbc
Mickaël Coiraton <mickael.coiraton@gmail.com>
2017-02-01 16:16:58 +0100

    modifié :         backup.sh
    	modifié :         backup.sh

--- backup.sh
+++ backup.sh
@@ -8,44 +8,54 @@
 # Updated: 18 December 2016
 #
 # This is a simple rotating backups tool based on rsync and SSH.
-# It backups selected directories from a remote machine to the local machine 
+# It backups selected directories from a remote host to the local machine 
 # which runs the script.
 #
 # ==============================================================================
 
-# Get backup config.
+# Source config file
 source backup.cfg
 
-# SSH keyfile checking.
+# Get incremental dates
+current_date=`date -I`
+last_date=`date -I -d "1 "$interval" ago"`
+
+# SSH keyfile checking
 if [ ! -f $ssh_key ]; then
   echo "Couldn't find ssh keyfile!"
   echo "Exiting..."
   exit 2
 fi
 
-# Rsync exclude parameter.
+# Rsync exclude parameter
 for dir in $exclude_dirs; do
-    exclude="$exclude --exclude=$dir"
+    exclude="$exclude --exclude='$dir'"
 done
 
-# Running verification and rsync for each source.
+# Running verification and rsync for each source
 for source in $sources; do
+    
+    # Extract source folder name
+    source_dir=${source##*/}
 
-    # Check remote permissions.
+    # Check remote permissions
     if ! ssh -i $ssh_key $r_user@$r_host "test -x $source"; then
         echo "Error with $source!"
-        echo "Target directory on remote machine doesn't exist or bad permissions."
+        echo "Target directory on remote machine doesn't exist or"\
+             "bad permissions."
         echo "Exiting..."
         exit 2
     fi
 
-    # Create local backup directory.
-    if [ ! -d $backup_dir/$source ]; then
-        mkdir -p $backup_dir/$source
+    # Create local backup directory
+    if [ ! -d $backup_dir/$current_date/$source_dir ]; then
+        mkdir -p $backup_dir/$current_date/$source_dir
     fi
-
-    # Run rsync.
-    rsync $exclude -a --delete -e "ssh -i $ssh_key" $r_user@$r_host:$source/ $backup_dir/$source/
+    
+    # Run rsync
+    rsync $exclude -av --delete -e "ssh -i $ssh_key" \
+          $r_user@$r_host:$source/ \
+          $backup_dir/$current_date/$source_dir
 
 done
 
